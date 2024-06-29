de0afb97128514d5b3a898e3e7d3a5488c9abe22
Zabotin Vladimir <ZabotinV78@gmail.com>
2024-06-29 12:22:33 +0500

    ИÑзучал введение из консоли данных

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Let's get started!");
+
+        System.out.print("Input your name: ");
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+        System.out.printf("Your name is %s\n", name);
     }
 }
