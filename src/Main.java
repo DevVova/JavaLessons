3ae7ce6c8f6100e6017929e2eac88e9d1f29aec5
Vladimir Zabotin <ZabotinV78@gmail.com>
2024-07-12 10:15:38 +0500

    next

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Vova! Снова повторение.");
+        System.out.println("Hello Vova! Остальные нахер!");
     }
 }
